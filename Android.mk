5c8738c1d7204ce86cea27948b6bf080b72c51c4
RealJohnGalt <johngalt@fake.mail>
2019-03-18 13:02:40 -0700

    [WIP] initial ali gsi bringup

---
+++ Android.mk
@@ -0,0 +1,89 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),ali_n)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := datastatusnotification
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/app/datastatusnotification/datastatusnotification.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := QtiTelephonyService
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/app/QtiTelephonyService/QtiTelephonyService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := HotwordEnrollmentOKGoogleCS47L35
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/priv-app/HotwordEnrollmentOKGoogleCS47L35/HotwordEnrollmentOKGoogleCS47L35.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := HotwordEnrollmentTGoogleCS47L35
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/priv-app/HotwordEnrollmentTGoogleCS47L35/HotwordEnrollmentTGoogleCS47L35.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := HotwordEnrollmentXGoogleCS47L35
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/priv-app/HotwordEnrollmentXGoogleCS47L35/HotwordEnrollmentXGoogleCS47L35.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ims
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/priv-app/ims/ims.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qcrilmsgtunnel
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/priv-app/qcrilmsgtunnel/qcrilmsgtunnel.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
+endif
---
+++ ali-vendor.mk
@@ -0,0 +1,36 @@
+PRODUCT_COPY_FILES += \
+	vendor/motorola/ali/proprietary/lib/libimscamera_jni.so:system/lib/libimscamera_jni.so \
+	vendor/motorola/ali/proprietary/lib/lib-imscamera.so:system/lib/lib-imscamera.so \
+	vendor/motorola/ali/proprietary/lib/libimsmedia_jni.so:system/lib/libimsmedia_jni.so \
+	vendor/motorola/ali/proprietary/lib/lib-imsvideocodec.so:system/lib/lib-imsvideocodec.so \
+	vendor/motorola/ali/proprietary/lib/lib-imsvtextutils.so:system/lib/lib-imsvtextutils.so \
+	vendor/motorola/ali/proprietary/lib/lib-imsvt.so:system/lib/lib-imsvt.so \
+	vendor/motorola/ali/proprietary/lib/lib-imsvtutils.so:system/lib/lib-imsvtutils.so \
+	vendor/motorola/ali/proprietary/lib/libqti-perfd-client_system.so:system/lib/libqti-perfd-client_system.so \
+	vendor/motorola/ali/proprietary/lib/libqti_performance.so:system/lib/libqti_performance.so \
+	vendor/motorola/ali/proprietary/lib/libqti-perfd-client_system.so:system/lib/libqti-perfd-client_system.so \
+	vendor/motorola/ali/proprietary/lib/vendor.qti.hardware.perf@1.0.so:system/lib/vendor.qti.hardware.perf@1.0.so \
+	vendor/motorola/ali/proprietary/lib/vendor.qti.imsrtpservice@1.0.so:system/lib/vendor.qti.imsrtpservice@1.0.so \
+	vendor/motorola/ali/proprietary/framework/cneapiclient.jar:system/framework/cneapiclient.jar \
+	vendor/motorola/ali/proprietary/framework/qcrilhook.jar:system/framework/qcrilhook.jar \
+	vendor/motorola/ali/proprietary/framework/qti-telephony-common.jar:system/framework/qti-telephony-common.jar \
+	vendor/motorola/ali/proprietary/framework/QtiTelephonyServicelibrary.jar:system/framework/QtiTelephonyServicelibrary.jar \
+	vendor/motorola/ali/proprietary/etc/perfservice.rc:system/etc/perfservice.rc \
+	vendor/motorola/ali/proprietary/etc/thermal-engine.conf:system/etc/thermal-engine.conf \
+	vendor/motorola/ali/proprietary/etc/permissions/com.motorola.camera2.xml:system/etc/permissions/com.motorola.camera2.xml \
+	vendor/motorola/ali/proprietary/etc/permissions/com.motorola.moto.xml:system/etc/permissions/com.motorola.moto.xml \
+	vendor/motorola/ali/proprietary/etc/permissions/privapp-permissions-com.android.hotwordenrollment.okgoogle.xml:system/etc/permissions/privapp-permissions-com.android.hotwordenrollment.okgoogle.xml \
+	vendor/motorola/ali/proprietary/etc/permissions/privapp-permissions-com.android.hotwordenrollment.tgoogle.xml:system/etc/permissions/privapp-permissions-com.android.hotwordenrollment.tgoogle.xml \
+	vendor/motorola/ali/proprietary/etc/permissions/privapp-permissions-com.android.hotwordenrollment.xgoogle.xml:system/etc/permissions/privapp-permissions-com.android.hotwordenrollment.xgoogle.xml \
+	vendor/motorola/ali/proprietary/etc/permissions/qcrilhook.xml:system/etc/permissions/qcrilhook.xml \
+	vendor/motorola/ali/proprietary/etc/permissions/telephonyservice.xml:system/etc/permissions/telephonyservice.xml
+
+PRODUCT_PACKAGES += \
+	CNEService \
+	ims \
+	datastatusnotification \
+	QtiTelephonyService \
+	HotwordEnrollmentOKGoogleCS47L35 \
+	HotwordEnrollmentTGoogleCS47L35 \
+	HotwordEnrollmentXGoogleCS47L35 \
+	qcrilmsgtunnel
