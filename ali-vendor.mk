0d4e2588240071fc2c95c9ff7f09613f0907268c
RealJohnGalt <johngalt@fake.mail>
2019-03-20 10:42:59 -0700

    more ims kanging

--- ali-vendor.mk
+++ ali-vendor.mk
@@ -9,12 +9,30 @@ PRODUCT_COPY_FILES += \
 	vendor/motorola/ali/proprietary/lib/libqti-perfd-client_system.so:system/lib/libqti-perfd-client_system.so \
 	vendor/motorola/ali/proprietary/lib/libqti_performance.so:system/lib/libqti_performance.so \
 	vendor/motorola/ali/proprietary/lib/libqti-perfd-client_system.so:system/lib/libqti-perfd-client_system.so \
+	vendor/motorola/ali/proprietary/lib/libdiag_system.so:system/lib/libdiag_system.so \
 	vendor/motorola/ali/proprietary/lib/vendor.qti.hardware.perf@1.0.so:system/lib/vendor.qti.hardware.perf@1.0.so \
 	vendor/motorola/ali/proprietary/lib/vendor.qti.imsrtpservice@1.0.so:system/lib/vendor.qti.imsrtpservice@1.0.so \
 	vendor/motorola/ali/proprietary/framework/cneapiclient.jar:system/framework/cneapiclient.jar \
 	vendor/motorola/ali/proprietary/framework/qcrilhook.jar:system/framework/qcrilhook.jar \
 	vendor/motorola/ali/proprietary/framework/qti-telephony-common.jar:system/framework/qti-telephony-common.jar \
 	vendor/motorola/ali/proprietary/framework/QtiTelephonyServicelibrary.jar:system/framework/QtiTelephonyServicelibrary.jar \
+	vendor/motorola/ali/proprietary/framework/com.quicinc.cne.api-V1.0-java.jar:system/framework/com.quicinc.cne.api-V1.0-java.jar \
+	vendor/motorola/ali/proprietary/framework/com.quicinc.cne.api-V1.1-java.jar:system/framework/com.quicinc.cne.api-V1.1-java.jar \
+	vendor/motorola/ali/proprietary/framework/com.quicinc.cne.constants-V1.0-java.jar:system/framework/com.quicinc.cne.constants-V1.0-java.jar \
+	vendor/motorola/ali/proprietary/framework/com.quicinc.cne.constants-V2.0-java.jar:system/framework/com.quicinc.cne.constants-V2.0-java.jar \
+	vendor/motorola/ali/proprietary/framework/com.quicinc.cne.constants-V2.1-java.jar:system/framework/com.quicinc.cne.constants-V2.1-java.jar \
+	vendor/motorola/ali/proprietary/framework/com.quicinc.cne.jar:system/framework/com.quicinc.cne.jar \
+	vendor/motorola/ali/proprietary/framework/com.quicinc.cne.server-V1.0-java.jar:system/framework/com.quicinc.cne.server-V1.0-java.jar \
+	vendor/motorola/ali/proprietary/framework/com.quicinc.cne.server-V2.0-java.jar:system/framework/com.quicinc.cne.server-V2.0-java.jar \
+	vendor/motorola/ali/proprietary/framework/com.quicinc.cne.server-V2.1-java.jar:system/framework/com.quicinc.cne.server-V2.1-java.jar \
+	vendor/motorola/ali/proprietary/framework/com.quicinc.cne.server-V2.2-java.jar:system/framework/com.quicinc.cne.server-V2.2-java.jar \
+	vendor/motorola/ali/proprietary/framework/qti-vzw-ims-internal.jar:system/framework/qti-vzw-ims-internal.jar \
+	vendor/motorola/ali/proprietary/framework/vendor.qti.data.factory-V1.0-java.jar:system/framework/vendor.qti.data.factory-V1.0-java.jar \
+	vendor/motorola/ali/proprietary/framework/vendor.qti.hardware.data.dynamicdds-V1.0-java.jar:system/framework/vendor.qti.hardware.data.dynamicdds-V1.0-java.jar \
+	vendor/motorola/ali/proprietary/framework/vendor.qti.hardware.data.latency-V1.0-java.jar:system/framework/vendor.qti.hardware.data.latency-V1.0-java.jar \
+	vendor/motorola/ali/proprietary/framework/vendor.qti.hardware.data.qmi-V1.0-java.jar:system/framework/vendor.qti.hardware.data.qmi-V1.0-java.jar \
+	vendor/motorola/ali/proprietary/framework/vendor.qti.ims.rcsconfig-V1.0-java.jar:system/framework/vendor.qti.ims.rcsconfig-V1.0-java.jar \
+	vendor/motorola/ali/proprietary/framework/vendor.qti.latency-V2.0-java.jar:system/framework/vendor.qti.latency-V2.0-java.jar \
 	vendor/motorola/ali/proprietary/etc/perfservice.rc:system/etc/perfservice.rc \
 	vendor/motorola/ali/proprietary/etc/thermal-engine.conf:system/etc/thermal-engine.conf \
 	vendor/motorola/ali/proprietary/etc/permissions/com.motorola.camera2.xml:system/etc/permissions/com.motorola.camera2.xml \
@@ -35,4 +53,5 @@ PRODUCT_PACKAGES += \
 	HotwordEnrollmentOKGoogleCS47L35 \
 	HotwordEnrollmentTGoogleCS47L35 \
 	HotwordEnrollmentXGoogleCS47L35 \
-	qcrilmsgtunnel
+	qcrilmsgtunnel \
+	ims-ext-common
